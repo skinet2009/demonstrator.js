5f502dcd78c0c4754a7e05fce3808d06b2d62efa
Nepritimov <skinet788767@gmail.com>
2019-02-08 01:01:06 +0300

    fix param

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import Demonstrator from './demonstrator';
 document.addEventListener('DOMContentLoaded', () => {
     const demonstrator = new Demonstrator({
         by: '.js-target',
-        document: this.document
+        document: document
     });
 
     demonstrator.init();
